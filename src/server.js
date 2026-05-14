77b8829909e7f59673647e26758bff219f5ab20d
Yuliia Shefner <yul.she91@gmail.com>
2026-05-14 12:58:46 +0200

    validation home work completed

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRouters from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -24,6 +25,7 @@ app.use(
 app.use(notesRouters);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
